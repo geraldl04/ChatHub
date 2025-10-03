91d99597c8d4348fc7bbc4416be9a17eddd73f48
geraldl04 <geraldi2004@outlook.com>
2025-10-03 12:23:14 +0200

    First Commit - Project Structure

---
+++ src/main/java/my/project/ChatHub/ChatHubApplication.java
@@ -0,0 +1,13 @@
+package my.project.ChatHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/project/ChatHub/ChatHubApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.ChatHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
